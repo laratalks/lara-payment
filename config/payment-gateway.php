0fdf51aa24f756d3e9982ff1dd88d5dc369672b0
Morteza Parvini <m.parvini@outlook.com>
2016-05-07 09:54:35 +0430

    build configs from file

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -5,24 +5,19 @@ return [
     'default_provider' => 'zarinpal',
 
     'providers' => [
-
-        'payline' => [
-            'api' => '', // payline API key
-            'gateway_send_url' => 'http://payline.ir/payment/gateway-send',
-            'gateway_verify_url' => 'http://payline.ir/payment/gateway-result-second',
-            'gateway_payment_url' => "http://payline.ir/payment/gateway-%d"
-        ],
-
         'zarinpal' => [
-            'merchant_id' => '', // Zarinpal Merchant Code
-            'wsdl' => 'https://www.zarinpal.com/pg/services/WebGate/wsdl',
-            'gateway_send_url' => '‫‪https://www.zarinpal.com/pg/StartPay/%s'
+            'api_key' => '', // Zarinpal Merchant Code
+        ],
+        
+        'upal' => [
+            'api_key' => '',
+            'gateway_id' => ''
         ]
 
     ],
 
     'proxy' => [
-        'enable' => false, // true, when you want send requests through proxy
+        'enabled' => false, // true, when you want send requests through proxy
         'type' => 'http', // available: http, https, socks5(only on non-wsdl requests)
         'use_credentials' => false, // true, if your proxy needs credentials
         'host' => 'localhost',
--- src/LaraPaymentServiceProvider.php
+++ src/LaraPaymentServiceProvider.php
@@ -2,6 +2,9 @@
 namespace Laratalks\LaraPayment;
 
 use Illuminate\Support\ServiceProvider;
+use Laratalks\PaymentGateways\Configs\Config;
+use Laratalks\PaymentGateways\Configs\ProviderConfig;
+use Laratalks\PaymentGateways\Configs\ProxyConfig;
 use Laratalks\PaymentGateways\GatewayManager;
 
 class LaraPaymentServiceProvider extends ServiceProvider 
@@ -21,8 +24,73 @@ class LaraPaymentServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('gateway-manager', function ($app) {
-             return new GatewayManager($app['config']['payment-gateways']);
+             return new GatewayManager($this->buildConfigs($app['config']['payment-gateways']));
         });
+    }
+
+    protected function buildConfigs(array $configs)
+    {
+        $config = new Config();
+
+        if (array_get($configs, 'default_provider') !== null) {
+            $config->setDefaultProvider(array_get($configs, 'default_provider'));
+        }
+
+        // set proxy configs
+        if (($proxy = array_get($configs, 'proxy')) !== null) {
+            $proxyConfig = new ProxyConfig();
+
+            if (array_get($proxy, 'enabled') !== null) {
+                $proxyConfig->setEnabled(array_get($proxy, 'enabled'));
+            }
+
+            if (array_get($proxy, 'type') !== null) {
+                $proxyConfig->setType(array_get($proxy, 'type'));
+            }
+
+            if (array_get($proxy, 'host') !== null) {
+                $proxyConfig->setHost(array_get($proxy, 'host'));
+            }
+
+            if (array_get($proxy, 'port') !== null) {
+                $proxyConfig->setPort(array_get($proxy, 'port'));
+            }
+
+            if (array_get($proxy, 'use_credentials') !== null) {
+                $proxyConfig->setUseCredentials(array_get($proxy, 'use_credentials'));
+            }
+
+            if (array_get($proxy, 'username') !== null) {
+                $proxyConfig->setUsername(array_get($proxy, 'username'));
+            }
+
+            if (array_get($proxy, 'password') !== null) {
+                $proxyConfig->setPassword(array_get($proxy, 'password'));
+            }
+
+            $config->setProxy($proxyConfig);
+        }
+
+
+        // set providers config
+        if (($providers = array_get($configs, 'providers'))  !== null && is_array($providers)) {
+            foreach ($providers as $name => $provider) {
+                $providerConfig = new ProviderConfig($name);
+
+                foreach ($provider as $key => $value) {
+                    if ($key === 'api_key') {
+                        $providerConfig->setApiKey($value);
+                        continue;
+                    }
+
+                    $providerConfig->set($key, $value);
+                }
+
+                $config->addProvider($providerConfig);
+            }
+        }
+        
+        return $config;
     }
 
     public function provides()
